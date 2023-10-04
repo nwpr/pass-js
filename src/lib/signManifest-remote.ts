5e982b93acceae853fdb1665b5fb33ac0606ce90
Nikolaus Walther <1459063+nwpr@users.noreply.github.com>
2023-10-04 18:35:38 +0200

    added remote signing

---
+++ src/lib/signManifest-remote.ts
@@ -0,0 +1,29 @@
+import fetch from 'node-fetch';
+
+const URL = process.env.URL || 'https://localhost:3000/api/sign-manifest';
+const HEADERS = process.env.HEADERS || {};
+HEADERS['Content-Type'] = 'application/json';
+
+/**
+ * Signs a manifest and returns the signature.
+ *
+ * @param {string} manifest - manifest to sign
+ * @returns {Buffer} - signature for given manifest
+ */
+export async function signManifest(
+  manifest: string
+): Promise<Buffer> {
+  return await fetch(URL, {
+    method: 'POST',
+    headers: HEADERS,
+    body: manifest,
+  }).then((res) => {
+    if (res.status !== 200) {
+      throw new Error(`Failed to sign manifest: ${res.statusText}`);
+    }
+    return res.buffer();
+  }
+    ).catch((err) => {
+        throw new Error(`Failed to sign manifest: ${err}`);
+    });
+}
